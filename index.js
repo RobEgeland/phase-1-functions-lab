bc90b27a1c2dc65cf39a8c7e6974f83bfc797c4a
Robert Egeland <robegeland@yahoo.com>
2021-12-15 18:42:34 -0600

    commit tree ff80ffdba648051dc27457983ddcb7b08df44f06

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    if(location < 42) {
+        return 42 - location
+    }
+    return location - 42
+}
+
+function distanceFromHqInFeet(location) {
+    distanceFromHqInBlocks(location);
+    return distanceFromHqInBlocks(location) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if(start < destination) {
+        return (destination - start) * 264
+    }
+    return (start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    distanceTravelledInFeet(start, destination);
+    if(distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    }
+    if(distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02
+    }
+    if(distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25
+    }
+    if(distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far'
+    }
+
+}
